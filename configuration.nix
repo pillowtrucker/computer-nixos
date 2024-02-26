9ba2f99d2622490a664e71ad78adfe1bb26653ab
pillowtrucker <githubino@janitor.chat>
2024-02-26 23:16:12 +0100

    broken mozc downloads

--- configuration.nix
+++ configuration.nix
@@ -71,7 +71,8 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-mozc
+      fcitx5-skk
+#      fcitx5-mozc
       fcitx5-table-other
       fcitx5-chinese-addons
       fcitx5-configtool
