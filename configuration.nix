4eba20c9acc9486078e6cbc45c3537c76ee9466f
pillowtrucker <githubino@janitor.chat>
2024-07-01 16:26:58 +0200

    enable protontricks

--- configuration.nix
+++ configuration.nix
@@ -518,6 +518,7 @@ in {
     enableSSHSupport = true;
   };
   programs.steam = {
+    protontricks.enable = true;
     enable = true;
     remotePlay.openFirewall =
       true; # Open ports in the firewall for Steam Remote Play
