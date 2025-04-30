2809a06d4d4ab86141c9cf88e010d7d81006bb76
pillowtrucker <githubino@janitor.chat>
2025-04-30 13:49:54 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -406,7 +406,7 @@ in {
         angband
         frotz
         netris
-        sfrotz
+        #        sfrotz
         tintin
         scummvm
         calibre
@@ -683,8 +683,8 @@ in {
     nvidiaSettings = true;
     dynamicBoost.enable = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    #    package = config.boot.kernelPackages.nvidiaPackages.production;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    #    package = config.boot.kernelPackages.nvidiaPackages.beta;
     #    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
